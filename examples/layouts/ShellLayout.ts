b807eb9a920604fca6841256e3a42f6b0833752a
crusa <artembazhenov2000@gmail.com>
2025-12-15 10:05:47 +0400

    refactor

--- examples/layouts/ShellLayout.ts
+++ examples/layouts/ShellLayout.ts
@@ -2,6 +2,9 @@ import {Feature, Layout, SlotsFeature} from "@icrusai/ts-router";
 import shell from "./shell.html?raw";
 
 export default class ShellLayout extends Layout {
+    @Feature(SlotsFeature)
+    public slots!: SlotsFeature<"header" | "sidebar" | "content">;
+
     protected renderStructure() {
         return shell;
     }
